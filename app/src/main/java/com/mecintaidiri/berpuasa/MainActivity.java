f6b387bac5ff05b7156ea1a64c73b78d89ec3b11
thoriq <aliecha79@gmail.com>
2024-04-24 07:33:22 +0700

    nav

--- app/src/main/java/com/mecintaidiri/berpuasa/MainActivity.java
+++ app/src/main/java/com/mecintaidiri/berpuasa/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mecintaidiri.berpuasa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -15,26 +18,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch (item.getItemId()){
 
                 case R.id.home:
+                    replaceFragment(new HomeFragment());
                     break;
 
                 case R.id.zakat:
+                    replaceFragment(new ZakatFragment());
                     break;
 
                 case R.id.donasi:
+                    replaceFragment(new DonasiFragment());
                     break;
 
                 case R.id.profil:
+                    replaceFragment(new ProfilFragment());
                     break;
             }
 
             return true;
         });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
 
     }
 }
